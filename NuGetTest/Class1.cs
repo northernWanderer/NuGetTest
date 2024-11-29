d73f9a147a4a294968faaea61304c4b96b23c6f4
Константин Александрович Самусик <103153852+northernWanderer@users.noreply.github.com>
2024-11-29 11:09:45 +0300

    Добавьте файлы проекта.

---
+++ NuGetTest/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace NuGetTest
+{
+    public class Class1
+    {
+        public void Log(string text)
+        {
+            using (FileStream fileStream = new FileStream("log.log", FileMode.OpenOrCreate))
+            {
+                using (StreamWriter writer = new StreamWriter(fileStream))
+                {
+                    writer.WriteLine(text);
+                }
+            }
+        }
+    }
+}
